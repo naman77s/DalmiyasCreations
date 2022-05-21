639ed31fe2bdaa35cc65133e62c7df4c7a193c4a
NAMHACK <70265763+naman77s@users.noreply.github.com>
2022-05-21 19:46:34 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 AOS.init();
 
+var sn = document.getElementById("Snav");
+function dump(){
+	sn.style.left = "-120%";
+}
+function pull(){
+	sn.style.left = "0";
+}
+
+
 var productA = {
 	name : "Matte Fabric" ,
 	color : "Blue / Green" ,
@@ -12,14 +21,51 @@ var productB = {
 	price : "80₹" ,
 	image : ["./assets/img/nw/B1.png" , "./assets/img/nw/B2.png" , "./assets/img/nw/G2.png"]
 };
-const name = document.getElementById("name");
-const price = document.getElementById("price");
+const cd = document.querySelector('.container-details');
+const name = cd.querySelector("#name");
+const price = cd.querySelector("#price");
 const buy = document.getElementById("Buy");
 const img = document.getElementsByClassName("image");
 const section = document.getElementById("section");
-var i = 0;
-	var user = prompt("🔷 What's your name?");
+const iname = document.getElementById("input-name");
+const prompt = document.getElementById("prompt");
+
+
+$(document).ready(function(){
+	const pct = document.querySelector('#p-a');
+		const img = pct.querySelectorAll(".p-i");
+		const set = document.querySelector('.setA');
+		const name = set.querySelectorAll("#name");
+		const price = set.querySelectorAll("#price");
+		name[0].innerHTML = "Type : " + productA.name ;
+		price[0].innerHTML = "Price :" + productA.price;
+		for (var i = 0, len = img.length; i < len; i++) {
+			img[i].setAttribute("src" , productA.image[i]);
+		}
+	}
+);
+$("#p-a").ready(function(){
+	const pct = document.querySelector('#p-b');
+		const img = pct.querySelectorAll(".p-i");
+		const set = document.querySelector('.setA');
+		const name = set.querySelectorAll("#name");
+		const price = set.querySelectorAll("#price");
+		name[1].innerHTML = "Type : " + productB.name ;
+		price[1].innerHTML = "Price :" + productB.price;
+		for (var i = 0, len = img.length; i < len; i++) {
+			img[i].setAttribute("src" , productB.image[i]);
+		}
+	}
+);
+
+
+function hidee(){
+	prompt.style.left = "-300%";
+	$("#prompt").hide(1000);
+}
+
 function openA(){
+	let user = iname.value;
 	name.innerHTML = "Type : " + productA.name + " |";
 	price.innerHTML = "|  Price :" + productA.price;
 	buy.setAttribute("href", "https://wa.me/918950661477?text=Hi%20Ankit,%20I'm%20" + user + "%20and%20am%20interested%20in%20your%20this%20product-%20*Name*:%20" + productA.name + "%20,%20*Color*:%20Any%20.%20Can%20you%20please%20provide%20me%20more%20details%20about%20it?%20");
@@ -27,8 +73,10 @@ function openA(){
 		img[i].setAttribute("src" , productA.image[i]);
 	}
 	section.style.bottom = "0";
+
 }
 function openB(){
+	let user = iname.value;
 	name.innerHTML = "Type : " + productB.name + " |";
 	price.innerHTML = "|  Price :" + productB.price;
 	buy.setAttribute("href", "https://wa.me/918950661477?text=Hi%20Ankit,%20I'm%20" + user + "%20and%20am%20interested%20in%20your%20product-%20*Name*:%20" + productB.name + "%20,%20*Color*:%20Any%20.%20Can%20you%20please%20provide%20me%20more%20details%20about%20it?%20");
@@ -37,6 +85,22 @@ function openB(){
 	}
 	section.style.bottom = "0";
 }
+
+
 function drop(){
 	section.style.bottom = "-120%";
 }
+
+
+
+$("a").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top - 200
+      }, 0, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
